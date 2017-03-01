f683ebd4fe0f02daeee955b326eb7a003b8cf8be
Zacharey Hollingshead <zach.hollingshead@gmail.com>
2017-03-01 09:36:02 -0600

    refactor(ux-input): Remove unused theme properties

--- src/input/ux-input-theme.ts
+++ src/input/ux-input-theme.ts
@@ -2,9 +2,4 @@ import {styles} from '../styles/decorators';
 
 @styles()
 export class UxInputTheme {
-  public background: string;
-  public foreground: string;
-
-  public backgroundDisabled: string;
-  public foregroundDisabled: string;
 }
