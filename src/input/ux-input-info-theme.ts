829e17c6868000d5d7aace5fe6e5b8b889e6ef06
Zacharey Hollingshead <zach.hollingshead@gmail.com>
2017-03-03 12:13:39 -0600

    style(ux-input): Filled out themes
    Code cleanup, preparing for pull reqeust.
    Remove required.
    Limit type to text and number.

--- src/input/ux-input-info-theme.ts
+++ src/input/ux-input-info-theme.ts
@@ -2,4 +2,6 @@ import {styles} from '../styles/decorators';
 
 @styles()
 export class UxInputInfoTheme {
+  public hintText: string;
+  public errorAccent: string;
 }
--- src/input/ux-input-theme.ts
+++ src/input/ux-input-theme.ts
@@ -2,4 +2,11 @@ import {styles} from '../styles/decorators';
 
 @styles()
 export class UxInputTheme {
+  public background: string = 'transparent';
+  public foreground: string;
+
+  public backgroundDisabled: string;
+  public foregroundDisabled: string;
+
+  public errorAccent: string;
 }
