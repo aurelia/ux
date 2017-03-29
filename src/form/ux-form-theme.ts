8ed6f83a698af20bb855f73ec07142495fdac8e7
Zacharey Hollingshead <zach.hollingshead@gmail.com>
2017-03-29 16:43:05 -0500

    feat(form): add ux form and form-row utility
    add ux-form component
    add form row layout class

---
+++ src/form/ux-form-theme.ts
@@ -0,0 +1,9 @@
+import { styles } from '../styles/decorators';
+
+@styles()
+export class UxFormTheme {
+  public background: string = 'transparent';
+  public foreground: string;
+
+  public formPadding: 8;
+}
