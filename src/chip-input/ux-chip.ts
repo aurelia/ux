3b732253a4bb458cf99080a60bc58d20dea14390
Zacharey Hollingshead <zach.hollingshead@gmail.com>
2017-04-08 01:15:15 -0500

    feat(chip): add closeable chip and final size

--- src/chip-input/ux-chip.ts
+++ src/chip-input/ux-chip.ts
@@ -1,4 +1,5 @@
 import { customElement, bindable, ViewResources, View, processAttributes } from 'aurelia-templating';
+import { DOM } from 'aurelia-pal';
 import { bindingMode } from 'aurelia-binding';
 import { inject } from 'aurelia-dependency-injection';
 import { StyleEngine } from '../styles/style-engine';
@@ -12,13 +13,14 @@ import { processDesignAttributes } from '../designs/design-attributes';
 export class UxChip implements Themable {
     @bindable public theme = null;
     @bindable public type: any;
+
     @bindable({ defaultBindingMode: bindingMode.twoWay })
     public value: any = undefined;
 
     public view: View;
 
     constructor(
-        /*private element: HTMLInputElement,*/
+        private element: HTMLInputElement,
         public resources: ViewResources,
         private styleEngine: StyleEngine) { }
 
@@ -34,5 +36,11 @@ export class UxChip implements Themable {
 
     public themeChanged(newValue: any) {
         this.styleEngine.applyTheme(this, newValue);
+    }
+
+    public closeChip() {
+        const closeEvent = DOM.createCustomEvent('close', { bubbles: false });
+
+        this.element.dispatchEvent(closeEvent);
     }
 }
