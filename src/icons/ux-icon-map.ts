09ff54973375e3451b26b554bf318a5f5d6c642f
Zacharey Hollingshead <zach.hollingshead@gmail.com>
2017-05-01 15:58:41 -0500

    feat(icon): add ux-icon component

---
+++ src/icons/ux-icon-map.ts
@@ -0,0 +1,5 @@
+export default class IconMap {
+    public static get Map() {
+        return [];
+    }
+}
---
+++ src/icons/ux-icon-theme.ts
@@ -0,0 +1,8 @@
+import {styles} from '../styles/decorators';
+
+@styles()
+export class UxIconTheme {
+  public size = '24px';
+
+  public foreground: string;
+}
