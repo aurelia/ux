605613d08157e11d102adc9e131178bcf985d04a
chrsmrtn- <chrsmrtn@outlook.com>
2017-08-02 08:28:29 -0700

    fix(ux-chip-input) made disabled and readonly string checks case insensitive

--- src/chip-input/ux-chip-input.ts
+++ src/chip-input/ux-chip-input.ts
@@ -53,7 +53,8 @@ export class UxChipInput implements Themable {
     }
 
     // ensure we cast empty string or 'disabled' as true
-    if (typeof this.disabled === 'string' && (this.disabled === '' || this.disabled === 'disabled')) {
+    if (typeof this.disabled === 'string' &&
+       (this.disabled === '' || this.disabled.toLocaleLowerCase() === 'disabled')) {
       this.disabled = true;
     }
 
@@ -64,7 +65,8 @@ export class UxChipInput implements Themable {
     }
 
     // ensure we cast empty string or 'readonly' as true
-    if (typeof this.readonly === 'string' && (this.readonly === '' || this.readonly === 'readonly')) {
+    if (typeof this.readonly === 'string' &&
+       (this.readonly === '' || this.readonly.toLocaleLowerCase() === 'readonly')) {
       this.readonly = true;
     }
 
@@ -170,7 +172,7 @@ export class UxChipInput implements Themable {
 
   public disabledChanged(newValue: boolean | string) {
     // ensure we cast empty string or 'disabled' as true
-    if (typeof newValue === 'string' && (newValue === '' || newValue === 'disabled')) {
+    if (typeof newValue === 'string' && (newValue === '' || newValue.toLocaleLowerCase() === 'disabled')) {
       newValue = true;
     }
 
@@ -187,7 +189,7 @@ export class UxChipInput implements Themable {
 
   public readonlyChanged(newValue: boolean | string) {
     // ensure we cast empty string or 'readonly' as true
-    if (typeof newValue === 'string' && (newValue === '' || newValue === 'readonly')) {
+    if (typeof newValue === 'string' && (newValue === '' || newValue.toLocaleLowerCase() === 'readonly')) {
       newValue = true;
     }
 
