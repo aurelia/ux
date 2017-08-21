add88b144bb80cfd23d8971699344a6a13d38548
chrsmrtn- <chrsmrtn@outlook.com>
2017-08-21 14:47:33 -0700

    converted to jsdoc comment style

--- src/resources/html-attributes.ts
+++ src/resources/html-attributes.ts
@@ -1,5 +1,12 @@
-// this function will ensure that we propertly treat a potential string value for a boolean attribute
-// as the boolean representation
+/**
+ * @description This function will ensure that we propertly treat a potential string value for a boolean attribute
+ * as the boolean representation
+ *
+ * @param {string} attributeName Name of the boolean attribute we are normalizing for
+ * @param {boolean|string} value Existing value of the boolean html attribute represented as a boolean or string
+ *
+ * @returns {boolean}
+ */
 export function normalizeBooleanAttribute(attributeName: string, value: boolean | string): boolean {
   let ret: boolean;
   if (typeof value === 'string') {
