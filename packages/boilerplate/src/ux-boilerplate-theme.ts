7f0276cddbccdc5c45c539c367a813c4d0fb7cbb
Zacharey Hollingshead <zach.hollingshead@gmail.com>
2017-11-22 12:25:23 -0600

    feat(boilerplate): add simple boilerplate
    gives a good starting point for new components
    
    resolves: #101

---
+++ packages/boilerplate/src/ux-boilerplate-theme.ts
@@ -0,0 +1,15 @@
+import { swatches, UxTheme } from '@aurelia-ux/core';
+
+export class UxBoilerplateTheme implements UxTheme {
+  public themeKey = 'boilerplate'; // the key for your components themes
+
+  /*
+   * The theming system will convert all of these properties to CSS variables.
+   *
+   * The naming convention for this is --ux-theme--key-property
+   *
+   * For example background would become --ux-theme--boilerplate-background
+   */
+
+  public background: string = swatches.white;
+}
---
+++ packages/boilerplate/src/ux-boilerplate.ts
@@ -0,0 +1,26 @@
+import { customElement, bindable } from 'aurelia-templating';
+import { inject } from 'aurelia-dependency-injection';
+import { StyleEngine, UxComponent } from '@aurelia-ux/core';
+import { UxBoilerplateTheme } from './ux-boilerplate-theme';
+
+const theme = new UxBoilerplateTheme();
+
+@inject(Element, StyleEngine)
+@customElement('ux-button')
+export class UxButton implements UxComponent {
+  @bindable public theme: UxBoilerplateTheme;
+
+  constructor(
+    public element: HTMLElement,
+    private styleEngine: StyleEngine) {
+      styleEngine.ensureDefaultTheme(theme);
+    }
+
+  public bind() {
+    this.themeChanged(this.theme);
+  }
+
+  public themeChanged(newValue: any) {
+    this.styleEngine.applyTheme(newValue, this.element);
+  }
+}
