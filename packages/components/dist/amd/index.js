d56f1de40e82fcba89be48ff48f69c9d6c4ee530
EisenbergEffect <rob@bluespire.com>
2017-12-21 10:38:28 -0800

    chore(all): prepare release 0.6.1

--- packages/components/dist/amd/index.js
+++ packages/components/dist/amd/index.js
@@ -1,18 +1,31 @@
-define(["require", "exports", "@aurelia-ux/button", "@aurelia-ux/checkbox", "@aurelia-ux/chip-input", "@aurelia-ux/datepicker", "@aurelia-ux/form", "@aurelia-ux/input", "@aurelia-ux/input-info", "@aurelia-ux/list", "@aurelia-ux/radio", "@aurelia-ux/textarea", "@aurelia-ux/switch"], function (require, exports, button_1, checkbox_1, chip_input_1, datepicker_1, form_1, input_1, input_info_1, list_1, radio_1, textarea_1, switch_1) {
+define(["require", "exports", "@aurelia-ux/button", "@aurelia-ux/checkbox", "@aurelia-ux/chip-input", "@aurelia-ux/datepicker", "@aurelia-ux/form", "@aurelia-ux/input", "@aurelia-ux/input-info", "@aurelia-ux/list", "@aurelia-ux/radio", "@aurelia-ux/textarea", "@aurelia-ux/switch", "@aurelia-ux/button", "@aurelia-ux/checkbox", "@aurelia-ux/chip-input", "@aurelia-ux/datepicker", "@aurelia-ux/form", "@aurelia-ux/input", "@aurelia-ux/input-info", "@aurelia-ux/list", "@aurelia-ux/radio", "@aurelia-ux/textarea", "@aurelia-ux/switch"], function (require, exports, button_1, checkbox_1, chip_input_1, datepicker_1, form_1, input_1, input_info_1, list_1, radio_1, textarea_1, switch_1, button_2, checkbox_2, chip_input_2, datepicker_2, form_2, input_2, input_info_2, list_2, radio_2, textarea_2, switch_2) {
     "use strict";
     Object.defineProperty(exports, "__esModule", { value: true });
+    exports.UxButtonTheme = button_1.UxButtonTheme;
+    exports.UxCheckboxTheme = checkbox_1.UxCheckboxTheme;
+    exports.UxChipInputTheme = chip_input_1.UxChipInputTheme;
+    exports.UxChipTheme = chip_input_1.UxChipTheme;
+    exports.UxTagTheme = chip_input_1.UxTagTheme;
+    exports.UxDatepickerTheme = datepicker_1.UxDatepickerTheme;
+    exports.UxFormTheme = form_1.UxFormTheme;
+    exports.UxInputTheme = input_1.UxInputTheme;
+    exports.UxInputInfoTheme = input_info_1.UxInputInfoTheme;
+    exports.UxListTheme = list_1.UxListTheme;
+    exports.UxRadioTheme = radio_1.UxRadioTheme;
+    exports.UxTextareaTheme = textarea_1.UxTextareaTheme;
+    exports.UxSwitchTheme = switch_1.UxSwitchTheme;
     function configure(config) {
-        button_1.configure(config);
-        checkbox_1.configure(config);
-        chip_input_1.configure(config);
-        datepicker_1.configure(config);
-        form_1.configure(config);
-        input_1.configure(config);
-        input_info_1.configure(config);
-        list_1.configure(config);
-        radio_1.configure(config);
-        textarea_1.configure(config);
-        switch_1.configure(config);
+        button_2.configure(config);
+        checkbox_2.configure(config);
+        chip_input_2.configure(config);
+        datepicker_2.configure(config);
+        form_2.configure(config);
+        input_2.configure(config);
+        input_info_2.configure(config);
+        list_2.configure(config);
+        radio_2.configure(config);
+        textarea_2.configure(config);
+        switch_2.configure(config);
     }
     exports.configure = configure;
 });
--- packages/components/dist/system/index.js
+++ packages/components/dist/system/index.js
@@ -18,38 +18,73 @@ System.register(["@aurelia-ux/button", "@aurelia-ux/checkbox", "@aurelia-ux/chip
     var button_1, checkbox_1, chip_input_1, datepicker_1, form_1, input_1, input_info_1, list_1, radio_1, textarea_1, switch_1;
     return {
         setters: [
-            function (button_1_1) {
-                button_1 = button_1_1;
+            function (button_2_1) {
+                exports_1({
+                    "UxButtonTheme": button_2_1["UxButtonTheme"]
+                });
+                button_1 = button_2_1;
             },
-            function (checkbox_1_1) {
-                checkbox_1 = checkbox_1_1;
+            function (checkbox_2_1) {
+                exports_1({
+                    "UxCheckboxTheme": checkbox_2_1["UxCheckboxTheme"]
+                });
+                checkbox_1 = checkbox_2_1;
             },
-            function (chip_input_1_1) {
-                chip_input_1 = chip_input_1_1;
+            function (chip_input_2_1) {
+                exports_1({
+                    "UxChipInputTheme": chip_input_2_1["UxChipInputTheme"],
+                    "UxChipTheme": chip_input_2_1["UxChipTheme"],
+                    "UxTagTheme": chip_input_2_1["UxTagTheme"]
+                });
+                chip_input_1 = chip_input_2_1;
             },
-            function (datepicker_1_1) {
-                datepicker_1 = datepicker_1_1;
+            function (datepicker_2_1) {
+                exports_1({
+                    "UxDatepickerTheme": datepicker_2_1["UxDatepickerTheme"]
+                });
+                datepicker_1 = datepicker_2_1;
             },
-            function (form_1_1) {
-                form_1 = form_1_1;
+            function (form_2_1) {
+                exports_1({
+                    "UxFormTheme": form_2_1["UxFormTheme"]
+                });
+                form_1 = form_2_1;
             },
-            function (input_1_1) {
-                input_1 = input_1_1;
+            function (input_2_1) {
+                exports_1({
+                    "UxInputTheme": input_2_1["UxInputTheme"]
+                });
+                input_1 = input_2_1;
             },
-            function (input_info_1_1) {
-                input_info_1 = input_info_1_1;
+            function (input_info_2_1) {
+                exports_1({
+                    "UxInputInfoTheme": input_info_2_1["UxInputInfoTheme"]
+                });
+                input_info_1 = input_info_2_1;
             },
-            function (list_1_1) {
-                list_1 = list_1_1;
+            function (list_2_1) {
+                exports_1({
+                    "UxListTheme": list_2_1["UxListTheme"]
+                });
+                list_1 = list_2_1;
             },
-            function (radio_1_1) {
-                radio_1 = radio_1_1;
+            function (radio_2_1) {
+                exports_1({
+                    "UxRadioTheme": radio_2_1["UxRadioTheme"]
+                });
+                radio_1 = radio_2_1;
             },
-            function (textarea_1_1) {
-                textarea_1 = textarea_1_1;
+            function (textarea_2_1) {
+                exports_1({
+                    "UxTextareaTheme": textarea_2_1["UxTextareaTheme"]
+                });
+                textarea_1 = textarea_2_1;
             },
-            function (switch_1_1) {
-                switch_1 = switch_1_1;
+            function (switch_2_1) {
+                exports_1({
+                    "UxSwitchTheme": switch_2_1["UxSwitchTheme"]
+                });
+                switch_1 = switch_2_1;
             }
         ],
         execute: function () {
