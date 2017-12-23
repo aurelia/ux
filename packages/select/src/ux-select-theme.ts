ee4cf679ed69c0014daedcad5c5870656c5f16f2
bigopon.777@gmail.com <bigopon.777@gmail.com>
2017-12-23 22:27:19 +1100

    btn full width, lighter item color

--- packages/select/src/ux-select-theme.ts
+++ packages/select/src/ux-select-theme.ts
@@ -3,10 +3,12 @@ import { swatches, UxTheme } from '@aurelia-ux/core';
 export class UxSelectTheme implements UxTheme {
   public themeKey = 'select';
 
-  public foreground: string = swatches.grey.p600;
+  public foreground: string = swatches.grey.p500;
   public background: string = 'transparent';
 
-  public triggerBorder: string = `1px solid ${swatches.grey.p600}`;
+  public triggerBorder: string = `1px solid ${swatches.grey.p500}`;
+  public triggerBorderDisabled: string = `1px solid ${swatches.grey.p400}`;
+  public triggerBorderFocused: string = `1px solid ${swatches.grey.p600}`;
 
   public listMaxWidth: number = 250;
   public listMaxWidthPx: string = '250px';
@@ -16,22 +18,15 @@ export class UxSelectTheme implements UxTheme {
   public listTransition: number = 200;
   public listTransitionS: string = '0.2s';
 
-  public valueCtMinWidth: number = 50;
+  public optionHover: string = swatches.grey.p300;
+  public optionSelected: string = swatches.grey.p400;
 
   public borderBottom: string = `1px solid ${swatches.grey.p500}`;
   public borderBottomHover: string = '1px solid var(--ux-design--accent)';
   public borderBottomSelected: string = '';
 
-  public diabledForeground: string = swatches.grey.p500;
-  public disabledBorderBottom: string = `1px dashed ${swatches.grey.p300}`;
+  public listboxShadow: string = 'rgba(0, 0, 0, 0.12)';
 
-  public fullWidthBorder: string = `1px solid ${swatches.grey.p200}`;
-  public fullWidthBackground: string = swatches.white;
-  public fullWidthBackgroundDisabled: string = swatches.grey.p200;
-
-  public selectListBoxShadow: string = 'rgba(0, 0, 0, 0.12)';
-
-  public selected: string = swatches.grey.p300;
 
   public error: string = swatches.red.p500;
 }
