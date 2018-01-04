13f7f7550bfa70c9fa8b1e8fe0799835c6db17a9
Zacharey Hollingshead <zach.hollingshead@gmail.com>
2018-01-04 07:37:33 -0600

    feat(grid): calculate width inside event handler & implement disposable

--- packages/grid/src/ux-responsive-utilities.ts
+++ packages/grid/src/ux-responsive-utilities.ts
@@ -1,48 +1,58 @@
 import { PLATFORM } from 'aurelia-pal';
+import { Disposable } from 'aurelia-binding';
 
 /** Utility class that assists with designing a responsive site. */
-export class UxResponsiveUtilities {
+export class UxResponsiveUtilities implements Disposable  {
+
+  /** Current screen height that can be used to set custom breakpoints. */
+  public height: number;
+
+  /** Current screen width that can be used to set custom breakpoints. */
   public width: number;
 
-  constructor() {
-    this.width = PLATFORM.global.innerWidth;
-
-    let recentlyUpdated = false;
-
-    window.addEventListener('resize', () => {
-      if (recentlyUpdated) {
-        return;
-      }
-
-      recentlyUpdated = true;
-      this.width = PLATFORM.global.innerWidth;
-
-      setTimeout(() => recentlyUpdated = false , 250);
-    });
-  }
-
   /** Visible on screens smaller than 480px. */
-  public get xs() {
-    return this.width <= 480;
-  }
+  public xs: boolean = false;
 
   /** Visible on screens larger than 480px, and smaller than 960px. */
-  public get sm() {
-    return this.width > 480 && this.width <= 960;
-  }
+  public sm: boolean = false;
 
   /** Visible on screens larger than 960px, and smaller than 1280px. */
-  public get md() {
-    return this.width > 960 && this.width <= 1280;
-  }
+  public md: boolean = false;
 
   /** Visible on screens larger than 1280px, and smaller than 1925px. */
-  public get lg() {
-    return this.width > 1280 && this.width <= 1925;
-  }
+  public lg: boolean = false;
 
   /** Visible on screens larger than 1925px. */
-  public get xl() {
-    return this.width > 1925;
+  public xl: boolean = false;
+
+  private recentlyUpdated = false;
+
+  constructor() {
+    window.addEventListener('resize', () => this.calculateResponsiveValues());
+
+    this.calculateResponsiveValues();
+  }
+
+  private calculateResponsiveValues() {
+    if (this.recentlyUpdated) {
+      return;
+    }
+
+    this.recentlyUpdated = true;
+
+    this.height = PLATFORM.global.innerHeight;
+    this.width = PLATFORM.global.innerWidth;
+
+    this.xs = this.width <= 480;
+    this.sm = this.width > 480 && this.width <= 960;
+    this.md = this.width > 960 && this.width <= 1280;
+    this.lg = this.width > 1280 && this.width <= 1925;
+    this.xl = this.width > 1925;
+
+    setTimeout(() => this.recentlyUpdated = false, 250);
+  }
+
+  public dispose() {
+    window.removeEventListener('resize', this.calculateResponsiveValues);
   }
 }
