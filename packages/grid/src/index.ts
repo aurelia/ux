3845dfaf43e7ab678dfd0cc4f105807cbea363fd
bigopon.777@gmail.com <bigopon.777@gmail.com>
2018-07-03 23:27:43 +1000

    fix(Grid): import & inline css

--- packages/grid/src/index.ts
+++ packages/grid/src/index.ts
@@ -1,9 +1,11 @@
-import { FrameworkConfiguration } from 'aurelia-framework';
+import { FrameworkConfiguration, DOM } from 'aurelia-framework';
 import { UxGrid } from './ux-grid';
+import css from './ux-grid.css';
 
 export { UxGridTheme } from './ux-grid-theme';
 export { UxResponsiveUtilities } from './ux-responsive-utilities';
 
 export function configure(config: FrameworkConfiguration) {
+  DOM.injectStyles(css, undefined, undefined, 'ux-grid-css');
   config.globalResources(UxGrid);
 }
