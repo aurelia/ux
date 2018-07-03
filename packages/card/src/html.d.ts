87af070e862445aee0af995969f540e23e5a0bc1
bigopon.777@gmail.com <bigopon.777@gmail.com>
2018-07-03 23:40:09 +1000

    fix(Card): import & inline css

--- packages/card/src/html.d.ts
+++ packages/card/src/html.d.ts
@@ -2,3 +2,8 @@ declare module '*.html' {
   const value: string;
   export default value;
 }
+
+declare module '*.css' {
+  const value: string;
+  export default value;
+}
--- packages/card/src/index.ts
+++ packages/card/src/index.ts
@@ -1,8 +1,10 @@
-import { FrameworkConfiguration } from 'aurelia-framework';
+import { FrameworkConfiguration, DOM } from 'aurelia-framework';
 import { UxCard } from './ux-card';
+import css from './ux-card.css';
 
 export { UxCardTheme } from './ux-card-theme';
 
 export function configure(config: FrameworkConfiguration) {
+  DOM.injectStyles(css, undefined, undefined, 'ux-card-css');
   config.globalResources(UxCard);
 }
