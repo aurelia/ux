f418410a6fe4f1ecd016534884bcad6b172dbbe1
bigopon.777@gmail.com <bigopon.777@gmail.com>
2018-07-03 23:41:56 +1000

    fix(Button): import & inline css

--- packages/button/src/index.ts
+++ packages/button/src/index.ts
@@ -1,8 +1,10 @@
-import { FrameworkConfiguration } from 'aurelia-framework';
+import { FrameworkConfiguration, DOM } from 'aurelia-framework';
 import { UxButton } from './ux-button';
+import css from './ux-button.css';
 
 export { UxButtonTheme } from './ux-button-theme';
 
 export function configure(config: FrameworkConfiguration) {
+  DOM.injectStyles(css, undefined, undefined, 'ux-button-css');
   config.globalResources(UxButton);
 }
