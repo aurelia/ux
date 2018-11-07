253fc77cec2428705dc875ea7a59a72365b50cde
Zach Hollingshead <zach.hollingshead@gmail.com>
2018-11-07 16:23:00 -0600

    feat(switch): update css to use BEM style and organize by specificity

--- packages/switch/src/ux-switch.ts
+++ packages/switch/src/ux-switch.ts
@@ -38,35 +38,33 @@ export class UxSwitch implements UxComponent {
   }
 
   public bind() {
-    const element = this.element;
-    const checkbox = this.checkbox;
-
-    if (element.hasAttribute('id')) {
-      const attributeValue = element.getAttribute('id');
+    if (this.element.hasAttribute('id')) {
+      const attributeValue = this.element.getAttribute('id');
 
       if (attributeValue != null) {
-        checkbox.setAttribute('id', attributeValue);
+        this.checkbox.setAttribute('id', attributeValue);
       }
     }
 
-    if (element.hasAttribute('tabindex')) {
-      const attributeValue = element.getAttribute('tabindex');
+    if (this.element.hasAttribute('tabindex')) {
+      const attributeValue = this.element.getAttribute('tabindex');
 
       if (attributeValue != null) {
-        checkbox.setAttribute('tabindex', attributeValue);
+        this.checkbox.setAttribute('tabindex', attributeValue);
       }
     }
 
-    if (element.hasAttribute('checked')) {
-      const attributeValue = element.getAttribute('checked');
+    if (this.element.hasAttribute('checked')) {
+      const attributeValue = this.element.getAttribute('checked');
 
       if (attributeValue || attributeValue === '') {
-        element.checked = true;
+        this.element.checked = true;
       }
     }
 
-    this.themeChanged(this.theme);
     this.disabledChanged(this.disabled);
+    this.valueChanged(this.value);
+    this.themeChanged(this.theme);
   }
 
   public attached() {
@@ -94,10 +92,25 @@ export class UxSwitch implements UxComponent {
     }
   }
 
+  public focusedChanged(newValue: boolean) {
+    if (newValue === true) {
+      this.element.classList.add('ux-switch--focused');
+    } else {
+      this.element.classList.remove('ux-switch--focused');
+    }
+  }
+
   public valueChanged(newValue: boolean) {
     if (this.ignoreValueChanges) {
       return;
     }
+
+    if (newValue === true) {
+      this.element.classList.add('ux-switch--checked');
+    } else {
+      this.element.classList.remove('ux-switch--checked');
+    }
+
     this.setChecked(newValue);
   }
 
@@ -110,6 +123,12 @@ export class UxSwitch implements UxComponent {
   }
 
   public disabledChanged(newValue: boolean | string) {
+    if (newValue === true) {
+      this.element.classList.add('ux-switch--disabled');
+    } else {
+      this.element.classList.remove('ux-switch--disabled');
+    }
+
     if (normalizeBooleanAttribute('disabled', newValue) && !this.element.classList.contains('disabled')) {
       this.checkbox.setAttribute('disabled', '');
     } else if (this.element.classList.contains('disabled')) {
