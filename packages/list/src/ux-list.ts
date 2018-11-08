6beac36c658cfb9fb814a1dfc8873df810ffbac8
Zach Hollingshead <zach.hollingshead@gmail.com>
2018-11-08 13:21:34 -0600

    feat(list): update css to use BEM styles and order by specificity

--- packages/list/src/ux-list.ts
+++ packages/list/src/ux-list.ts
@@ -8,6 +8,7 @@ import { UxListTheme } from './ux-list-theme';
 
 export class UxList implements UxComponent {
   @bindable public theme: UxListTheme;
+  @bindable public type: string;
 
   constructor(
     public element: HTMLElement,
@@ -16,6 +17,18 @@ export class UxList implements UxComponent {
   public bind() {
     if (this.theme != null) {
       this.themeChanged(this.theme);
+    }
+
+    this.typeChanged(this.type);
+  }
+
+  public typeChanged(newValue: string, oldValue?: string) {
+    if (typeof oldValue === 'string') {
+      this.element.classList.remove(`ux-list--${oldValue}`);
+    }
+
+    if (typeof newValue === 'string') {
+      this.element.classList.add(`ux-list--${newValue}`);
     }
   }
 
