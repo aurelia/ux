98bd27b3616a75d2891174bd7679895a56bfe52f
Zach Hollingshead <zach.hollingshead@gmail.com>
2018-12-18 10:07:58 -0600

    refactor(chip-input): remove dead code

--- packages/chip-input/src/ux-tag-theme.ts
+++
@@ -1,8 +0,0 @@
-import { UxTheme } from '@aurelia-ux/core';
-
-export class UxTagTheme implements UxTheme {
-  public themeKey: string = 'tag';
-
-  public background: string;
-  public foreground: string;
-}
