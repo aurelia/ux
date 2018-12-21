3ac32d16ad5ca76ad0e2572a9e83b85d7342c413
Zach Hollingshead <zach.hollingshead@gmail.com>
2018-12-20 20:43:47 -0600

    fix(chip-input): remove unused tag theme

--- packages/chip-input/src/index.ts
+++ packages/chip-input/src/index.ts
@@ -1,7 +1,6 @@
 import { FrameworkConfiguration, PLATFORM } from 'aurelia-framework';
 
 export { UxChipInputTheme } from './ux-chip-input-theme';
-export { UxTagTheme } from './ux-tag-theme';
 export { UxChipTheme } from './ux-chip-theme';
 
 export function configure(config: FrameworkConfiguration) {
