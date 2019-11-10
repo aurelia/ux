88da199c46d9c49fb23b4a93a98b68e8d15f8e20
bigopon.777@gmail.com <bigopon.777@gmail.com>
2019-11-10 14:37:01 +1100

    feat(button): export button, use static export

--- packages/button/src/index.ts
+++ packages/button/src/index.ts
@@ -1,9 +1,10 @@
-import { FrameworkConfiguration, PLATFORM } from 'aurelia-framework';
+import { FrameworkConfiguration } from 'aurelia-framework';
+import { UxButton } from './ux-button';
 
 export { UxButtonTheme } from './ux-button-theme';
 
 export function configure(config: FrameworkConfiguration) {
-  config.globalResources([
-    PLATFORM.moduleName('./ux-button')
-  ]);
+  config.globalResources(UxButton);
 }
+
+export { UxButton }
