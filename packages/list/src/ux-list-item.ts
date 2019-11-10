60a5818ce24d79408f18359599f3665f5ac5be5c
bigopon.777@gmail.com <bigopon.777@gmail.com>
2019-11-10 23:25:07 +1100

    chore(build): refactor ux-list build

--- packages/list/src/ux-list-item.ts
+++ packages/list/src/ux-list-item.ts
@@ -1,6 +1,8 @@
-import { customElement, bindable } from 'aurelia-templating';
+import { customElement, bindable, inlineView } from 'aurelia-templating';
+import * as VIEW from './ux-list-item.html';
 
 @customElement('ux-list-item')
+@inlineView(VIEW)
 export class UxListItem {
   @bindable public theme = null;
 }
