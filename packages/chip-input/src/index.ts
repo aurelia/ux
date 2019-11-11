5cd010c17365057c4d834ccaa75116d4d259fcea
bigopon.777@gmail.com <bigopon.777@gmail.com>
2019-11-12 00:35:08 +1100

    chore(build): refactor chip-input build

--- packages/chip-input/src/index.ts
+++ packages/chip-input/src/index.ts
@@ -1,11 +1,14 @@
-import { FrameworkConfiguration, PLATFORM } from 'aurelia-framework';
+/// <reference path="html.d.ts" />
+import { FrameworkConfiguration } from 'aurelia-framework';
+import { UxChip } from './ux-chip';
+import { UxChipInput } from './ux-chip-input';
 
 export { UxChipInputTheme } from './ux-chip-input-theme';
 export { UxChipTheme } from './ux-chip-theme';
 
 export function configure(config: FrameworkConfiguration) {
   config.globalResources([
-    PLATFORM.moduleName('@aurelia-ux/chip-input/ux-chip-input'),
-    PLATFORM.moduleName('@aurelia-ux/chip-input/ux-chip')
+    UxChipInput,
+    UxChip
   ]);
 }
