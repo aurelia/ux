da9c5ce0c7e49550c79c9568dcfe7040931c7452
bigopon.777@gmail.com <bigopon.777@gmail.com>
2019-11-11 23:17:48 +1100

    chore(build): refactor grid build

--- packages/grid/src/index.ts
+++ packages/grid/src/index.ts
@@ -1,11 +1,15 @@
-import { FrameworkConfiguration, PLATFORM } from 'aurelia-framework';
+/// <reference path="html.d.ts" />
+import { FrameworkConfiguration } from 'aurelia-framework';
+import { UxGrid } from './ux-grid';
+import { UxGridCell } from './ux-grid-cell';
 
 export { UxResponsiveUtilities } from './ux-responsive-utilities';
 export { UxGridTheme } from './ux-grid-theme';
+export { UxGrid, UxGridCell };
 
 export function configure(config: FrameworkConfiguration) {
   config.globalResources([
-    PLATFORM.moduleName('@aurelia-ux/grid/ux-grid'),
-    PLATFORM.moduleName('@aurelia-ux/grid/ux-grid-cell')
+    UxGrid,
+    UxGridCell
   ]);
 }
