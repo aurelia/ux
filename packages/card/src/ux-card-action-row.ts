0f62777c5fd9ba4febd756d3c3d21515698463ce
bigopon.777@gmail.com <bigopon.777@gmail.com>
2019-11-12 00:53:35 +1100

    chore(build): refactor card build

--- packages/card/src/ux-card-action-row.ts
+++ packages/card/src/ux-card-action-row.ts
@@ -1,5 +1,7 @@
-import { customElement } from 'aurelia-templating';
+import { customElement, inlineView } from 'aurelia-templating';
+import * as UX_CARD_ACTION_ROW from './ux-card-action-row.html';
 
 @customElement('ux-card-action-row')
+@inlineView(UX_CARD_ACTION_ROW)
 export class UxCardActionRow {
 }
--- packages/card/src/ux-card-footer.ts
+++ packages/card/src/ux-card-footer.ts
@@ -1,5 +1,7 @@
-import { customElement } from 'aurelia-templating';
+import { customElement, inlineView } from 'aurelia-templating';
+import * as UX_CARD_FOOTER from './ux-card-footer.html';
 
 @customElement('ux-card-footer')
+@inlineView(UX_CARD_FOOTER)
 export class UxCardFooter {
 }
