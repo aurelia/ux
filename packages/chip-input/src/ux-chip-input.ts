194be8bd3f399995dbdf945bbc69cc61c29bd7bb
Ben Girardet <ben@platform5.ch>
2019-11-21 19:04:35 +0100

    feat(chip-input): focus api

--- packages/chip-input/src/ux-chip-input.ts
+++ packages/chip-input/src/ux-chip-input.ts
@@ -1,6 +1,6 @@
 import { customElement, bindable, inlineView } from 'aurelia-templating';
 import { DOM } from 'aurelia-pal';
-import { bindingMode } from 'aurelia-binding';
+import { bindingMode, observable } from 'aurelia-binding';
 import { inject } from 'aurelia-dependency-injection';
 import { StyleEngine, UxComponent, normalizeBooleanAttribute } from '@aurelia-ux/core';
 import { UxChipInputTheme } from './ux-chip-input-theme';
@@ -22,11 +22,16 @@ export class UxChipInput implements UxComponent {
   @bindable({ defaultBindingMode: bindingMode.twoWay })
   public chips: string[] = new Array<string>();
 
+  @observable
+  public focused: boolean = false;
+
   private textbox: HTMLInputElement;
   private chiprepeat: Element;
   private tagrepeat: Element;
 
-  constructor(private element: HTMLElement, private styleEngine: StyleEngine) { }
+  constructor(private element: HTMLElement, private styleEngine: StyleEngine) {
+    Object.setPrototypeOf(element, uxChipInputElementProto);
+  }
 
   public bind() {
     this.themeChanged(this.theme);
@@ -58,31 +63,31 @@ export class UxChipInput implements UxComponent {
   }
 
   public attached() {
-    const blurEvent = DOM.createCustomEvent('blur', { bubbles: true });
+    // const blurEvent = DOM.createCustomEvent('blur', { bubbles: true });
 
-    this.textbox.addEventListener('focus', () => {
-      this.element.classList.add('ux-chip-input--focused');
-    });
+    // this.textbox.addEventListener('focus', () => {
+    //   this.element.classList.add('ux-chip-input--focused');
+    // });
 
-    this.textbox.addEventListener('blur', () => {
-      this.addChip();
-      this.element.classList.remove('ux-chip-input--focused');
-      this.element.dispatchEvent(blurEvent);
-    });
+    // this.textbox.addEventListener('blur', () => {
+    //   this.addChip();
+    //   this.element.classList.remove('ux-chip-input--focused');
+    //   this.element.dispatchEvent(blurEvent);
+    // });
   }
 
   public detached() {
-    const blurEvent = DOM.createCustomEvent('blur', { bubbles: true });
+    // const blurEvent = DOM.createCustomEvent('blur', { bubbles: true });
 
-    this.textbox.removeEventListener('focus', () => {
-      this.element.classList.add('ux-chip-input--focused');
-    });
+    // this.textbox.removeEventListener('focus', () => {
+    //   this.element.classList.add('ux-chip-input--focused');
+    // });
 
-    this.textbox.removeEventListener('blur', () => {
-      this.addChip();
-      this.element.classList.remove('ux-chip-input--focused');
-      this.element.dispatchEvent(blurEvent);
-    });
+    // this.textbox.removeEventListener('blur', () => {
+    //   this.addChip();
+    //   this.element.classList.remove('ux-chip-input--focused');
+    //   this.element.dispatchEvent(blurEvent);
+    // });
   }
 
   public handleKeyup(event: KeyboardEvent) {
@@ -181,4 +186,76 @@ export class UxChipInput implements UxComponent {
 
     this.styleEngine.applyTheme(newValue, this.element);
   }
+
+  public focusedChanged(focused: boolean) {
+    if (focused === true) {
+      this.element.classList.add('ux-chip-input--focused');
+    } else {
+      this.element.classList.remove('ux-chip-input--focused');
+    }
+
+    this.element.dispatchEvent(DOM.createCustomEvent(focused ? 'focus' : 'blur', { bubbles: false }));
+  }
+
+  public focus() {
+    this.textbox.focus();
+  }
+
+  public blur() {
+    if (document.activeElement === this.textbox) {
+      this.textbox.blur();
+      this.addChip();
+    }
+  }
+
+  public getValue() {
+    return this.value;
+  }
+
+  public setValue(value: any) {
+    const oldValue = this.value;
+    let newValue = value;
+
+    if (typeof newValue === 'string') {
+      newValue = newValue.split(this.separator);
+    }
+
+    function arraysEqual(arr1: Array<any>, arr2: Array<any>) {
+      if (arr1.length !== arr2.length) {
+        return false;
+      }
+      for (let i = arr1.length; i--;) {
+        if (arr1[i] !== arr2[i]) {
+          return false;
+        }
+      }
+      return true;
+    }
+
+    if (!arraysEqual(oldValue, newValue)) {
+      this.value = newValue;
+      this.element.dispatchEvent(DOM.createCustomEvent('change', { bubbles: true }));
+    }
+  }
 }
+
+const getVm = <T>(_: any) => _.au.controller.viewModel as T;
+const uxChipInputElementProto = Object.assign(
+  Object.create(HTMLElement.prototype, {
+  value: {
+    get() {
+      return getVm<UxChipInput>(this).getValue();
+    },
+    set(value: any) {
+      getVm<UxChipInput>(this).setValue(value);
+    }
+  },
+  }), {
+    focus() {
+      getVm<UxChipInput>(this).focused = true;
+    },
+    blur() {
+      getVm<UxChipInput>(this).focused = false;
+    }
+  }
+);
