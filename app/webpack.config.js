dcc71c46797acf8c7349d15fcdba7fd852daab45
bigopon.777@gmail.com <bigopon.777@gmail.com>
2019-12-02 00:37:57 +1100

    chore(example): update example app config

--- app/webpack.config.js
+++ app/webpack.config.js
@@ -11,23 +11,41 @@ module.exports = function({ production = '' } = {}) {
   return {
     mode: production === 'production' ? 'production' : 'development',
     resolve: {
+      // symlinks: true,
       extensions: ['.ts', '.js'],
       modules: [
         path.resolve(__dirname, 'src'),
         path.resolve(__dirname, 'node_modules'),
         // root node modules for common aurelia packages
-        path.resolve(__dirname, '../node_modules')
+        // path.resolve(__dirname, '../packages/core'),
+        // path.resolve(__dirname, '../packages/datepicker')
       ],
       alias: {
         'src': path.resolve(__dirname, 'src'),
+        // '@aurelia-ux': path.resolve(__dirname, '../packages'),
         // alias all aurelia packages to parent node_modules,
         // so packages & core modules will use the same version of core modules
         ...([
+          'polyfills',
+          'dependency-injection',
           'loader',
           'pal',
           'pal-browser',
           'metadata',
-          'logging'
+          'logging',
+          'binding',
+          'path',
+          'framework',
+          'history',
+          'history-browser',
+          'event-aggregator',
+          'router',
+          'route-recognizer',
+          'templating',
+          'templating-binding',
+          'templating-resources',
+          'templating-router',
+          'task-queue',
         ].reduce(
           /**
            * @param {Record<string, string>} map
@@ -39,31 +57,33 @@ module.exports = function({ production = '' } = {}) {
           },
           {}
         )),
+        // moment: path.resolve(__dirname, './node_modules/moment'),
         // alias all packages to src code
-        // ...([
-        //   'button',
-        //   'card',
-        //   'checkbox',
-        //   'chip-input',
-        //   'core',
-        //   'datepicker',
-        //   'form',
-        //   'grid',
-        //   'icons',
-        //   'input',
-        //   'input-info',
-        //   'list',
-        //   'radio',
-        //   'select',
-        //   'slider',
-        //   'switch',
-        //   'textarea',
-        // ].reduce((map, packageName) => {
-        //   const mappedPackagedName = `@aurelia-ux/${packageName}`;
-        //   map[mappedPackagedName] = path.resolve(__dirname, `../packages/${packageName}/src`)
-        //   return map;
-        // }, {}))
-      }
+        ...([
+          'button',
+          'card',
+          'checkbox',
+          'chip-input',
+          'core',
+          'datepicker',
+          'form',
+          'grid',
+          'icons',
+          'input',
+          'input-info',
+          'list',
+          'radio',
+          'select',
+          'slider',
+          'switch',
+          'textarea',
+        ].reduce((map, packageName) => {
+          const mappedPackagedName = `@aurelia-ux/${packageName}`;
+          map[mappedPackagedName] = path.resolve(__dirname, `../packages/${packageName}/src/`);
+          return map;
+        }, {}))
+      },
+
     },
     entry: {
       app: './src/main.ts'
@@ -79,24 +99,41 @@ module.exports = function({ production = '' } = {}) {
           loader: 'html-loader'
         },
         {
-          test: /\.css$/,
-          loader: 'css-loader'
-        }
+          test: /\.css$/i,
+          issuer: [{ not: [{ test: /\.html$/i }] }],
+          use: ['style-loader', 'css-loader']
+        },
+        {
+          test: /\.css$/i,
+          issuer: [{ test: /\.html$/i }],
+          // CSS required in templates cannot be extracted safely
+          // because Aurelia would try to require it again in runtime
+          use: ['css-loader']
+        },
       ]
     },
     plugins: [
       new AureliaWebpackPlugin.AureliaPlugin({
-        config: [
-          "defaultBindingLanguage",
-          // "router",
-          // "history",
-          "defaultResources",
-          "eventAggregator",
-          "developmentLogging",
-        ],
+        // config: [
+        //   "defaultBindingLanguage",
+        //   // "router",
+        //   // "history",
+        //   "defaultResources",
+        //   "eventAggregator",
+        //   "developmentLogging",
+        // ],
         aureliaApp: undefined,
         entry: undefined,
         dist: 'es2015',
+        // noModulePathResolve: true,
+      }),
+      new AureliaWebpackPlugin.ModuleDependenciesPlugin({
+        "@aurelia-ux/datepicker": [
+          "./ux-calendar.html",
+          "./ux-datepicker.html",
+          "./ux-picker-dialog.html",
+          "./ux-year-list.html",
+        ],
       }),
       new HtmlWebpackPlugin({
 
