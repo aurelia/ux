ec8fe5e260d31dcc40e4892edd46748898676a34
bigopon.777@gmail.com <bigopon.777@gmail.com>
2019-12-02 00:17:16 +1100

    chore(input-info): revert to multi-modules build

--- packages/input-info/src/ux-input-info.ts
+++ packages/input-info/src/ux-input-info.ts
@@ -1,16 +1,10 @@
-import { customElement, bindable, inlineView } from 'aurelia-templating';
+import { customElement, bindable } from 'aurelia-templating';
 import { inject } from 'aurelia-dependency-injection';
 import { UxInputInfoTheme } from './ux-input-info-theme';
 import { StyleEngine, UxComponent } from '@aurelia-ux/core';
-import VIEW from './ux-input-info.html';
-import { PLATFORM } from 'aurelia-pal';
 
 @inject(Element, StyleEngine)
 @customElement('ux-input-info')
-@inlineView(
-  VIEW,
-  [PLATFORM.moduleName('@aurelia-ux/input-info/ux-input-info.css')]
-)
 export class UxInputInfo implements UxComponent {
   @bindable public target: Element;
   @bindable public uxInputCounter = null;
