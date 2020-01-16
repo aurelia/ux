3a57e1d0a9bb62c64e294216a1df35f3abb9b6fe
Ben Girardet <ben@platform5.ch>
2020-01-16 23:42:27 +0100

    fix(components): register comp as plugin

--- packages/components/src/index.ts
+++ packages/components/src/index.ts
@@ -1,4 +1,4 @@
-import { FrameworkConfiguration } from 'aurelia-framework';
+import { FrameworkConfiguration, PLATFORM } from 'aurelia-framework';
 
 export { UxCardTheme } from '@aurelia-ux/card';
 export { UxButtonTheme } from '@aurelia-ux/button';
@@ -24,36 +24,20 @@ export {
 } from '@aurelia-ux/select';
 export { UxSliderTheme, UxSlider, UxSliderElement } from '@aurelia-ux/slider';
 
-import { configure as configureButtonComponent } from '@aurelia-ux/button';
-import { configure as configureCardComponent } from '@aurelia-ux/card';
-import { configure as configureCheckboxComponent } from '@aurelia-ux/checkbox';
-import { configure as configureChipComponent } from '@aurelia-ux/chip-input';
-import { configure as configureDatepickerComponent } from '@aurelia-ux/datepicker';
-import { configure as configureGridComponent } from '@aurelia-ux/grid';
-import { configure as configureFormComponent } from '@aurelia-ux/form';
-import { configure as configureInputComponent } from '@aurelia-ux/input';
-import { configure as configureInputInfoComponent } from '@aurelia-ux/input-info';
-import { configure as configureListComponent } from '@aurelia-ux/list';
-import { configure as configureRadioComponent } from '@aurelia-ux/radio';
-import { configure as configureTextareaComponent } from '@aurelia-ux/textarea';
-import { configure as configureSwitchComponent } from '@aurelia-ux/switch';
-import { configure as configureSelectComponent } from '@aurelia-ux/select';
-import { configure as configureSliderComponent } from '@aurelia-ux/slider';
-
 export function configure(config: FrameworkConfiguration) {
-  configureButtonComponent(config);
-  configureCardComponent(config);
-  configureCheckboxComponent(config);
-  configureChipComponent(config);
-  configureDatepickerComponent(config);
-  configureGridComponent(config);
-  configureFormComponent(config);
-  configureInputComponent(config);
-  configureInputInfoComponent(config);
-  configureListComponent(config);
-  configureRadioComponent(config);
-  configureTextareaComponent(config);
-  configureSwitchComponent(config);
-  configureSelectComponent(config);
-  configureSliderComponent(config);
+  config.plugin(PLATFORM.moduleName('@aurelia-ux/button'));
+  config.plugin(PLATFORM.moduleName('@aurelia-ux/card'));
+  config.plugin(PLATFORM.moduleName('@aurelia-ux/checkbox'));
+  config.plugin(PLATFORM.moduleName('@aurelia-ux/chip-input'));
+  config.plugin(PLATFORM.moduleName('@aurelia-ux/datepicker'));
+  config.plugin(PLATFORM.moduleName('@aurelia-ux/grid'));
+  config.plugin(PLATFORM.moduleName('@aurelia-ux/form'));
+  config.plugin(PLATFORM.moduleName('@aurelia-ux/input'));
+  config.plugin(PLATFORM.moduleName('@aurelia-ux/input-info'));
+  config.plugin(PLATFORM.moduleName('@aurelia-ux/list'));
+  config.plugin(PLATFORM.moduleName('@aurelia-ux/radio'));
+  config.plugin(PLATFORM.moduleName('@aurelia-ux/textarea'));
+  config.plugin(PLATFORM.moduleName('@aurelia-ux/switch'));
+  config.plugin(PLATFORM.moduleName('@aurelia-ux/select'));
+  config.plugin(PLATFORM.moduleName('@aurelia-ux/slider'));
 }
