eabcd36df1ffb9b1528b20ecf8a53ec1c9c5df20
Ben Girardet <ben@platform5.ch>
2020-02-04 21:03:36 +0100

    feat(app): test generic ux component

--- app/src/routes.ts
+++ app/src/routes.ts
@@ -5,6 +5,7 @@ export let routes = [
 
   { route: 'theming', moduleId: PLATFORM.moduleName('theming'), name: 'theming', title: 'Theming', nav: true },
   { route: 'components', moduleId: PLATFORM.moduleName('components'), name: 'components', title: 'Components', nav: true },
+  { route: 'ux-generic-component', moduleId: PLATFORM.moduleName('ux-generic-component'), name: 'ux-generic-component', title: 'Generic Component', nav: true },
   { route: 'showcase', moduleId: PLATFORM.moduleName('showcase'), name: 'showcase', title: 'Showcase', nav: true },
   { route: 'demo', moduleId: PLATFORM.moduleName('demo'), name: 'demo', title: 'Demo (old)', nav: true }
 ];
---
+++ app/src/ux-generic-component.ts
@@ -0,0 +1,9 @@
+export class UxGenericComponent {
+  public variant = 'filled';
+  public showIcons = false;
+  public status = '';
+  public fontSize = '16px';
+  public labelFontSize = '12px';
+  public height = 'auto';
+  public style = 'normal';
+}
