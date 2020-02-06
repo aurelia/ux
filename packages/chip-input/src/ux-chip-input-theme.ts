7e1e6c43e026678e8dd3dc3c47cc16e516d70dc5
Ben Girardet <ben@platform5.ch>
2020-02-06 14:13:44 +0100

    fix(chip-input): complete theme properties

--- packages/chip-input/src/ux-chip-input-theme.ts
+++ packages/chip-input/src/ux-chip-input-theme.ts
@@ -3,6 +3,26 @@ import {  UxTheme } from '@aurelia-ux/core';
 export class UxChipInputTheme implements UxTheme {
   public themeKey: string = 'chip-input';
 
-  public foreground: string;
-  public bottomBorder: string;
+  public foreground?: string;
+  public foregroundLabel?: string;
+  public background?: string;
+  public backgroundHover?: string;
+  public activeColor?: string;
+
+  public fontSize?: string;
+  public letterSpacing?: string;
+  public labelFontSize?: string;
+  public labelLetterSpacing?: string;
+
+  public borderRadius?: string;
+  public borderWidth?: string;
+
+  public borderBottomWidth?: string;
+  public borderBottomColor?: string;
+  public borderActiveWidth?: string;
+
+  public disabledForeground?: string;
+  public disabledBackground?: string;
+
+  public error?: string;
 }
