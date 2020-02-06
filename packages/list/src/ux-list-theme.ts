29a3367b1eff23365d060aa1388ddc337397ef38
Ben Girardet <ben@platform5.ch>
2020-02-06 22:27:59 +0100

    feat(list): add choice selected state css

--- packages/list/src/ux-list-theme.ts
+++ packages/list/src/ux-list-theme.ts
@@ -3,8 +3,9 @@ import { UxTheme } from '@aurelia-ux/core';
 export class UxListTheme implements UxTheme {
   public themeKey = 'list';
 
-  public foreground: string;
-  public secondaryForeground: string;
+  public foreground?: string;
+  public secondaryForeground?: string;
 
-  public background: string;
+  public background?: string;
+  public backgroundSelected?: string;
 }
