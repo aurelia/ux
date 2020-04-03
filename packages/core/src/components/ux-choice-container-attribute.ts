4914f16c5a2b11e152c2428b60c465ce5517b1e3
Ben Girardet <ben@platform5.ch>
2020-04-03 08:11:21 +0200

    doc(choice): comment the valueChanged

--- packages/core/src/components/ux-choice-container-attribute.ts
+++ packages/core/src/components/ux-choice-container-attribute.ts
@@ -86,11 +86,17 @@ export class UxChoiceContainerAttribute {
     if (this.multiple === 'auto') {
       this.multipleChanged(); // call this to ensure isMultiple respect value type
     }
+    // Before to process the value, let's make
+    // it's the proper type according to `isMultiple`
     if (this.isMultiple && typeof newValue === 'string') {
       this.value = [];
     } else if (!this.isMultiple && Array.isArray(newValue)) {
       this.value = undefined;
     }
+    // By using requestProcessValue we avoid too many
+    // process in case the value changes quickly
+    // This can happen if the value type must be fixed (above)
+    // or if there are several choice registration in a row
     this.requestProcessValue();
   }
 
