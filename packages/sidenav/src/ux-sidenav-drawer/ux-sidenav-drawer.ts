bc01cf49b7f5279a1bbdfef658753171232ea7b3
Maxim Balaganskiy <m.balaganskiy@gmail.com>
2020-05-14 11:27:43 +1000

    feat(sidenav): add default configuration and a modal breakpoint

--- packages/sidenav/src/ux-sidenav-drawer/ux-sidenav-drawer.ts
+++ packages/sidenav/src/ux-sidenav-drawer/ux-sidenav-drawer.ts
@@ -1,41 +1,38 @@
 import { useView, customElement, PLATFORM, inject, bindable } from "aurelia-framework";
 import { normalizeBooleanAttribute } from "@aurelia-ux/core";
+import { UxDefaultSidenavConfiguration } from "../ux-default-sidenav-configuration";
 
 export const OPEN_CHANGED = 'open-changed';
 
-@inject(Element)
+@inject(Element, UxDefaultSidenavConfiguration)
 @customElement('ux-sidenav-drawer')
 @useView(PLATFORM.moduleName('./ux-sidenav-drawer.html'))
 export class UxSidenavDrawer {
-  constructor(public element: HTMLElement) { }
+  constructor(public element: HTMLElement, private defaultConfig: UxDefaultSidenavConfiguration) { }
 
   @bindable
   side: 'left' | 'right' | 'bottom' = 'left';
 
   public openBoolean: boolean = false;
   @bindable
-  open: boolean | string;
+  open: boolean | string = false;
   openChanged() {
     this.openBoolean = normalizeBooleanAttribute('open', this.open);
     this.element.dispatchEvent(new CustomEvent(OPEN_CHANGED, { detail: this.openBoolean }));
   }
 
   @bindable
-  over: boolean | string = false;
+  over: boolean | string = this.defaultConfig.over;
 
   isOver(): boolean {
     return normalizeBooleanAttribute('over', this.over);
   }
 
-  public backdropBoolean: boolean = false;
   @bindable
-  backdrop: boolean | string;
-  backdropChanged() {
-    this.backdropBoolean = normalizeBooleanAttribute('backdrop', this.backdrop);
-  }
+  backdrop: boolean | string = this.defaultConfig.backdrop;
 
   isBackdrop(): boolean {
-    return this.backdropBoolean;
+    return normalizeBooleanAttribute('backdrop', this.backdrop);
   }
 
   toggle() {
--- packages/sidenav/src/ux-sidenav/ux-sidenav.ts
+++ packages/sidenav/src/ux-sidenav/ux-sidenav.ts
@@ -1,19 +1,30 @@
 import { customElement, useView, inject, TaskQueue, PLATFORM, bindable } from "aurelia-framework";
 import { UxSidenavDrawer, OPEN_CHANGED } from "../ux-sidenav-drawer/ux-sidenav-drawer";
 import { UxSidenavTheme } from "./ux-sidenav-theme";
-import { StyleEngine } from "@aurelia-ux/core";
+import { StyleEngine, normalizeNumberAttribute } from "@aurelia-ux/core";
+import { IUxSidenavDrawerElement } from "../ux-sidenav-drawer/i-ux-sidenav-drawer-element";
+import { UxDefaultSidenavConfiguration } from "../ux-default-sidenav-configuration";
 
-@inject(Element, StyleEngine, TaskQueue)
+@inject(Element, StyleEngine, TaskQueue, UxDefaultSidenavConfiguration)
 @customElement('ux-sidenav')
 @useView(PLATFORM.moduleName('./ux-sidenav.html'))
 export class UxSidenav {
-  constructor(public element: HTMLElement, private styleEngine: StyleEngine, private taskQueue: TaskQueue) { }
+  constructor(public element: HTMLElement, private styleEngine: StyleEngine, private taskQueue: TaskQueue,
+    private defaultConfig: UxDefaultSidenavConfiguration) { }
 
-  backdrop: boolean = false;
+  backdrop: boolean = this.defaultConfig.backdrop;
+  width: number;
 
-  leftDrawer: UxSidenavDrawer;
-  rightDrawer: UxSidenavDrawer;
-  bottomDrawer: UxSidenavDrawer;
+  leftDrawer: UxSidenavDrawer | undefined;
+  rightDrawer: UxSidenavDrawer | undefined;
+  bottomDrawer: UxSidenavDrawer | undefined;
+
+  private _modalBreakpoint: number = this.defaultConfig.modalBreakpoint;
+  @bindable
+  public modalBreakpoint: number | string = this.defaultConfig.modalBreakpoint;
+  modalBreakpointChanged() {
+    this._modalBreakpoint = normalizeNumberAttribute(this.modalBreakpoint) ?? 0;
+  }
 
   @bindable
   public theme: UxSidenavTheme;
@@ -26,19 +37,19 @@ export class UxSidenav {
   }
 
   attached() {
-    this.leftDrawer = (this.element.querySelector('ux-sidenav-drawer[side="left"]') as any)?.au['ux-sidenav-drawer']?.viewModel;
-    this.rightDrawer = (this.element.querySelector('ux-sidenav-drawer[side="right"]') as any)?.au['ux-sidenav-drawer'].viewModel;
-    this.bottomDrawer = (this.element.querySelector('ux-sidenav-drawer[side="bottom"]') as any)?.au['ux-sidenav-drawer'].viewModel;
+    this.leftDrawer = this.element.querySelector<IUxSidenavDrawerElement>('ux-sidenav-drawer[side="left"]')?.au.controller.viewModel;
+    this.rightDrawer = this.element.querySelector<IUxSidenavDrawerElement>('ux-sidenav-drawer[side="right"]')?.au.controller.viewModel;
+    this.bottomDrawer = this.element.querySelector<IUxSidenavDrawerElement>('ux-sidenav-drawer[side="bottom"]')?.au.controller.viewModel;
     if (this.leftDrawer) {
-      this.updateMargin(this.leftDrawer);
+      this.updatePadding(this.leftDrawer);
       this.leftDrawer.element.addEventListener(OPEN_CHANGED, this.leftDrawerOpenChanged);
     }
     if (this.rightDrawer) {
-      this.updateMargin(this.rightDrawer);
+      this.updatePadding(this.rightDrawer);
       this.rightDrawer.element.addEventListener(OPEN_CHANGED, this.rightDrawerOpenChanged);
     }
     if (this.bottomDrawer) {
-      this.updateMargin(this.bottomDrawer);
+      this.updatePadding(this.bottomDrawer);
       this.bottomDrawer.element.addEventListener(OPEN_CHANGED, this.bottomDrawerOpenChanged);
     }
     this.taskQueue.queueTask(() => this.element.classList.add('ux-sidenav--transition'));
@@ -56,12 +67,15 @@ export class UxSidenav {
     }
   }
 
-  leftDrawerOpenChanged = () => this.updateMargin(this.leftDrawer);
-  rightDrawerOpenChanged = () => this.updateMargin(this.rightDrawer);
-  bottomDrawerOpenChanged = () => this.updateMargin(this.bottomDrawer);
+  leftDrawerOpenChanged = () => this.updatePadding(this.leftDrawer!);
+  rightDrawerOpenChanged = () => this.updatePadding(this.rightDrawer!);
+  bottomDrawerOpenChanged = () => this.updatePadding(this.bottomDrawer!);
 
-  updateMargin(drawer: UxSidenavDrawer) {
-    const size = drawer.openBoolean && !drawer.isOver() ? `${drawer.side === 'bottom' ? drawer.element.clientHeight : drawer.element.clientWidth}px` : '';
+  updatePadding(drawer: UxSidenavDrawer) {
+    const isModal = this.width <= this._modalBreakpoint;
+    const size = drawer.openBoolean && !drawer.isOver() && !isModal
+      ? `${drawer.side === 'bottom' ? drawer.element.clientHeight : drawer.element.clientWidth}px`
+      : '';
     switch (drawer.side) {
       case 'left':
         this.element.style.paddingLeft = size;
@@ -73,19 +87,20 @@ export class UxSidenav {
         this.element.style.paddingBottom = size;
         break;
     }
-    this.backdrop = this.leftDrawer?.openBoolean && this.leftDrawer?.isBackdrop()
-      || this.rightDrawer?.openBoolean && this.rightDrawer?.isBackdrop()
-      || this.bottomDrawer?.openBoolean && this.bottomDrawer?.isBackdrop();
+    this.backdrop = this.leftDrawer?.openBoolean && (this.leftDrawer?.isBackdrop() || isModal)
+      || this.rightDrawer?.openBoolean && (this.rightDrawer?.isBackdrop() || isModal)
+      || !!this.bottomDrawer?.openBoolean && (this.bottomDrawer?.isBackdrop() || isModal);
   }
 
   close() {
-    if (this.leftDrawer?.isBackdrop()) {
+    const isModal = this.width <= this._modalBreakpoint;
+    if (this.leftDrawer && (this.leftDrawer.isBackdrop() || isModal)) {
       this.leftDrawer.open = false;
     }
-    if (this.rightDrawer?.isBackdrop()) {
+    if (this.rightDrawer && (this.rightDrawer.isBackdrop() || isModal)) {
       this.rightDrawer.open = false;
     }
-    if (this.bottomDrawer?.isBackdrop()) {
+    if (this.bottomDrawer && (this.bottomDrawer.isBackdrop() || isModal)) {
       this.bottomDrawer.open = false;
     }
   }
