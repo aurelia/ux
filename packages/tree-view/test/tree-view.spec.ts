1010e64d9cc8eb636ae6df7906be218fb7b213b3
Maxim Balaganskiy <m.balaganskiy@gmail.com>
2020-05-29 12:10:50 +1000

    fix(tree-view): remove artificial test

--- packages/tree-view/test/tree-view.spec.ts
+++ packages/tree-view/test/tree-view.spec.ts
@@ -3,16 +3,16 @@ import { UxTreeView } from '../src/index';
 import { Container, BindingLanguage } from 'aurelia-framework';
 import { TemplatingBindingLanguage } from 'aurelia-templating-binding';
 
-// describe('@aurelia-ux/tree-view', () => {
-//   describe('tree-view.spec.ts', () => {
-//     it('binds/unbinds correctly', () => {
-//       const container = new Container();
-//       const host = document.createElement('div');
-//       container.registerInstance(Element, host);
-//       container.registerInstance(BindingLanguage, TemplatingBindingLanguage);
-//       const uxTreeView = container.get(UxTreeView);
+describe('@aurelia-ux/tree-view', () => {
+  describe('tree-view.spec.ts', () => {
+    it('exports', () => {
+      // const container = new Container();
+      // const host = document.createElement('div');
+      // container.registerInstance(Element, host);
+      // container.registerInstance(BindingLanguage, TemplatingBindingLanguage);
+      // const uxTreeView = container.get(UxTreeView);
 
-//       expect(uxTreeView).toBeDefined();
-//     });
-//   });
-// });
+      expect(UxTreeView).toBeDefined();
+    });
+  });
+});
