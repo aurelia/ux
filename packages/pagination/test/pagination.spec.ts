aef2cdeeffdd1ed54b115d888083b4ee3a963012
Maxim Balaganskiy <m.balaganskiy@gmail.com>
2020-06-15 10:27:30 +1000

    feat(pagination): add basic test

--- packages/pagination/test/pagination.spec.ts
+++ packages/pagination/test/pagination.spec.ts
@@ -4,19 +4,36 @@ import { Container } from 'aurelia-framework';
 
 describe('@aurelia-ux/lookup', () => {
   describe('pagination.spec.ts', () => {
-    it('binds/unbinds correctly', () => {
+    it('builds page links correctly', () => {
       const container = new Container();
       const host = document.createElement('div');
       container.registerInstance(Element, host);
       const uxPagination = container.get(UxPagination);
 
-      spyOn(uxPagination, 'pagesChanged');
-      spyOn(uxPagination, 'visiblePagesChanged');
+      uxPagination.pages = 100;
+      uxPagination.visiblePages = 10;
 
       uxPagination.bind();
 
-      expect(uxPagination.pagesChanged).toHaveBeenCalledTimes(1);
-      expect(uxPagination.visiblePagesChanged).toHaveBeenCalledTimes(1);
+      expect(uxPagination.pageLinks.length).toEqual(10);
     });
+
+    it('moves visible links correctly', () => {
+      const container = new Container();
+      const host = document.createElement('div');
+      container.registerInstance(Element, host);
+      const uxPagination = container.get(UxPagination);
+
+      uxPagination.pages = 100;
+      uxPagination.visiblePages = 10;
+      uxPagination.activePage = 50;
+
+      uxPagination.bind();
+
+      expect(uxPagination.pageLinks.length).toEqual(10);
+      expect(uxPagination.pageLinks[0]).toEqual(45);
+      expect(uxPagination.pageLinks[9]).toEqual(54);
+    });
+
   });
 });
